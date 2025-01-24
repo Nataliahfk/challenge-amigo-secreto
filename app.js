76f3da75a0c0280653b48e4ea99419f6f0b59538
Natalia <hnataliaf@gmail.com>
2025-01-24 15:10:44 -0300

    crea correcciones

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// array para almacenar os nombres de los amigos
+// array para almacenar los nombres de los amigos
 let amigos =[];
 
 // función para agregar amigos
 function agregarAmigo(){
-    let inputAmigo = document.getElementById('amigo').value.trim();
-    let nombreAmigo = inputAmigo; /*El método trim elimina los espacios en blanco en 
+    const inputAmigo = document.getElementById('amigo');
+    const nombreAmigo = inputAmigo.ariaValueMax.trim(); /*El método trim elimina los espacios en blanco en 
                                                           ambos extremos del string*/
 }     
 
@@ -25,23 +25,22 @@ if(amigos.includes(nombreAmigo)){
 amigos.push(nombreAmigo);
 
 // Limpiar el campo de entrada
-function limpiarInput (){
-    document.querySelector('#amigo').value = '';
-}
+inputAmigo.ariaValu = "";    
+    
 // Actualizar la lista en el HTML
 actualizarLista();    
 
 // funcion para actualizar la lista amigos de la interfaz
 function actualizarLista(){
-    let listaAmigos = document.getElementById('listaAmigos');
+    const listaAmigos = document.getElementById('listaAmigos');
 
     // Limpiar el contenido actual de la lista
     listaAmigos.innerHTML = ""; // Borra cualquier contenido previo dentro del contenedor de la lista
 
     // Recorrer el array con ciclo for
     for(let i = 0 ; i < amigos.length; i++){
-        let arrayAmigos = document.createElement('arrayAmigos');
-        arrayAmigos.textContent = amigos[i];
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
         listaAmigos.appendChild(arrayAmigos);
     }
 }
@@ -50,17 +49,17 @@ function actualizarLista(){
 // función para seleccionar un amigo aleatorio
 function sortearAmigo(){
     // Validar que haya amigos disponibles
-    if(amigos.length=== 0){ // Comprueba si el array 'amigos' esta vacio
+    if(amigos.length === 0){ // Comprueba si el array 'amigos' esta vacio
         alert("No hay amigos disponibles para sortear. Agrega al menos uno");
     } return;
 
     // Genera un indice aleatorio
-    let indiceAleatorio = Math.floor(Math.random()*amigos.length); /*genera un número aleatorio entre 0 y el 
+    const indiceAleatorio = Math.floor(Math.random()*amigos.length); /*genera un número aleatorio entre 0 y el 
                                                                     largo del array*/ 
     
-    let amigoSorteado = amigos[indiceAleatorio]; // Usa el indice aleatorio para obtener el nombre de la lista                                                                    
+    const amigoSorteado = amigos[indiceAleatorio]; // Usa el indice aleatorio para obtener el nombre de la lista                                                                    
     
     //Mostrar el resultado en el HTML
-    let resultado = document.getElementById('resultado');
+    const resultado = document.getElementById('resultado');
     resultado.innerHTML = `Amigo sorteado: <strong> ${amigoSorteado} </strong>`;
 }
